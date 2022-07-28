0ca303073b55e78255321d504b7729be65a84b13
Erik Enriquez <erik.enriquez01@utrgv.edu>
2022-07-27 22:08:35 -0500

    initial empty files

---
+++ Lab5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class BrowserHistory {
+	public:
+		string* history;
+		int thing() {
+			return 7;
+		}
+};
---
+++ tests.cpp
@@ -0,0 +1,15 @@
+// test file - do not modify
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include <ctime>
+using namespace std;
+
+#include "Lab5.cpp"
+
+void makeObj() {
+    BrowserHistory test();
+}
+
+
+TEST_CASE("setup", "[y]") {
+};
